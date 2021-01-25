0eaf3fa2d32c64571767d34a79a56caae34318e9
ranadheer Kandhi <ranadheerkandhi@ranadheers-MacBook-Pro.local>
2021-01-25 09:47:05 -0500

    Switching select functionality from radio to multiSelect options

--- src/Features/ActiveMetrics/sliceReducer.js
+++ src/Features/ActiveMetrics/sliceReducer.js
@@ -1,22 +1,20 @@
-import {
-    createSlice
-} from 'redux-starter-kit';
+import { createSlice } from 'redux-starter-kit';
 
 const initialState = {
-    selectedMetrics: [],
+  selectedMetrics: [],
 };
 
 const slice = createSlice({
-    name: 'selectedMetrics',
-    initialState,
-    reducers: {
-        active: (state, action) => {
-            state.selectedMetrics = [...state.selectedMetrics, action.payload];
-        },
-        remove: (state, action) => {
-            state.selectedMetrics = state.selectedMetrics.filter(element => element.metricName !== action.payload);
-        },
+  name: 'selectedMetrics',
+  initialState,
+  reducers: {
+    active: (state, action) => {
+      state.selectedMetrics = [...state.selectedMetrics, action.payload];
     },
+    remove: (state, action) => {
+      state.selectedMetrics = state.selectedMetrics.filter(element => element.metricName !== action.payload);
+    },
+  },
 });
 
 export const reducer = slice.reducer;
--- src/components/Switches.js
+++ src/components/Switches.js
@@ -3,140 +3,91 @@ import Switch from '@material-ui/core/Switch';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
-import {useDispatch, useSelector} from 'react-redux';
-import {actions} from '../Features/ActiveMetrics/sliceReducer';
-import {makeStyles} from "@material-ui/core/styles";
+import { useDispatch, useSelector } from 'react-redux';
+import { actions } from '../Features/ActiveMetrics/sliceReducer';
+import { makeStyles } from '@material-ui/core/styles';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
-const useStyles = makeStyles({
-    container: {
-        padding: '10px 20px',
-    },
-});
+const useStyles = makeStyles(theme => ({
+  container: {
+    padding: '10px 20px',
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+}));
 export default function Switches() {
-    const classes = useStyles();
-    const [state, setState] = React.useState({
-        checkedA: false,
-        checkedB: false,
-        checkedC: false,
-        checkedD: false,
-        checkedE: false,
-        checkedF: false,
-    });
+  const classes = useStyles();
+  const metricsOptions = [
+    { name: 'INJ Valve Open', value: 'injValveOpen' },
+    { name: 'Oil Temp', value: 'oilTemp' },
+    { name: 'Tubing Pressure', value: 'tubingPressure' },
+    { name: 'Flare Temp', value: 'flareTemp' },
+    { name: 'Casing Pressure', value: 'casingPressure' },
+    { name: 'Water Temp', value: 'waterTemp' },
+  ];
 
-    const timeStamp = useSelector(state => state.heartbeat);
-    const dispatch = useDispatch();
-    const activeArr = useSelector(state => state.activeMetrics.selectedMetrics);
+  const [metric, setSetMetrics] = React.useState([]);
+  const activeArr = useSelector(state => state.activeMetrics.selectedMetrics);
 
-    const handleChange = name => event => {
-        const metric = event.target.value;
-        const isChecked = event.target.checked;
-        setState({...state, [name]: event.target.checked});
+  const handleChange = event => {
+    const { value } = event.target;
+    const metricName = value ? value[value.length - 1] : null;
 
-        if (isChecked) {
-            dispatch(
-                actions.active({
-                    metricName: metric,
-                    before: timeStamp.current,
-                    after: timeStamp.past,
-                }),
-            );
-        } else {
-            const metricIndex = activeArr.find(element => element.metricName === metric);
-            dispatch(actions.remove(metricIndex.metricName));
+    const selectedMetricOption = metricsOptions.filter(i => i.name === metricName);
+    const metricValue = selectedMetricOption.length > 0 ? selectedMetricOption[0].value : '';
+    setSetMetrics(value);
+
+    if (activeArr.some(metric => metric.metricName === metricValue) || metricValue === '') {
+      var updatedMetricValues = [];
+      for (var i = 0; i < metricsOptions.length; i++) {
+        for (var y = 0; y < value.length; y++) {
+          if (metricsOptions[i].name === value[y]) {
+            updatedMetricValues.push(metricsOptions[i]);
+          }
         }
-    };
+      }
+      var uniqueMetrics = activeArr.filter(item => !updatedMetricValues.some(other => item.metricName === other.value));
 
-    return (
-        <div className={classes.container}>
-            <h1>Select Metrics</h1>
-            <FormControl component="fieldset">
-                <FormGroup aria-label="position" row>
-                    <FormControlLabel
-                        value="top"
-                        control={
-                            <Switch
-                                checked={state.checkedA}
-                                onChange={handleChange('checkedA')}
-                                value="injValveOpen"
-                                color="primary"
-                                inputProps={{'aria-label': 'primary checkbox'}}
-                            />
-                        }
-                        label="INJ Valve Open"
-                        labelPlacement="top"
-                    />
-                    <FormControlLabel
-                        value="start"
-                        control={
-                            <Switch
-                                checked={state.checkedB}
-                                onChange={handleChange('checkedB')}
-                                value="oilTemp"
-                                color="primary"
-                                inputProps={{'aria-label': 'primary checkbox'}}
-                            />
-                        }
-                        label="Oil Temp"
-                        labelPlacement="top"
-                    />
-                    <FormControlLabel
-                        value="bottom"
-                        control={
-                            <Switch
-                                checked={state.checkedC}
-                                onChange={handleChange('checkedC')}
-                                value="tubingPressure"
-                                color="primary"
-                                inputProps={{'aria-label': 'primary checkbox'}}
-                            />
-                        }
-                        label="Tubing Pressure"
-                        labelPlacement="top"
-                    />
-                    <FormControlLabel
-                        value="end"
-                        control={
-                            <Switch
-                                checked={state.checkedD}
-                                onChange={handleChange('checkedD')}
-                                value="flareTemp"
-                                color="primary"
-                                inputProps={{'aria-label': 'primary checkbox'}}
-                            />
-                        }
-                        label="Flare Temp"
-                        labelPlacement="top"
-                    />
-                    <FormControlLabel
-                        value="end"
-                        control={
-                            <Switch
-                                checked={state.checkedE}
-                                onChange={handleChange('checkedE')}
-                                value="casingPressure"
-                                color="primary"
-                                inputProps={{'aria-label': 'primary checkbox'}}
-                            />
-                        }
-                        label="Casing Pressure"
-                        labelPlacement="top"
-                    />
-                    <FormControlLabel
-                        value="end"
-                        control={
-                            <Switch
-                                checked={state.checkedF}
-                                onChange={handleChange('checkedF')}
-                                value="waterTemp"
-                                color="primary"
-                                inputProps={{'aria-label': 'primary checkbox'}}
-                            />
-                        }
-                        label="Water Temp"
-                        labelPlacement="top"
-                    />
-                </FormGroup>
-            </FormControl>
-        </div>
-    );
+      return dispatch(actions.remove(uniqueMetrics[0].metricName));
+    } else {
+      return dispatch(
+        actions.active({
+          metricName: metricValue,
+          before: timeStamp.current,
+          after: timeStamp.past,
+        }),
+      );
+    }
+  };
+
+  const timeStamp = useSelector(state => state.heartbeat);
+  const dispatch = useDispatch();
+
+  return (
+    <div className={classes.container}>
+      <h1>Select Metrics</h1>
+      <FormControl component="fieldset">
+        <FormGroup aria-label="position" row>
+          <FormControl className={classes.formControl}>
+            <Select
+              value={metric}
+              onChange={handleChange}
+              multiple
+              className={classes.selectEmpty}
+              inputProps={{ 'aria-label': 'Without label' }}
+            >
+              {metricsOptions.map(option => (
+                <MenuItem value={option.name} key={option.value}>
+                  {option.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </FormGroup>
+      </FormControl>
+    </div>
+  );
 }
